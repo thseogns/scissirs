d0be0de4d48b09d4e66164c83676e9c63a53bc20
thseogns <99688960+thseogns@users.noreply.github.com>
2022-11-17 15:12:36 +0900

    Update Box.jsx

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -5,7 +5,7 @@ const Box = (props) => {
     let result ;
     if(props.title == 'Computer' && props.result !=='tie' && props.result !==''){
         //컴퓨터, 비기지 않았고, 결과값이 비어있지 않을 때 
-        result = props.result == 'win' ? 'lose' : 'win';
+        result = props.result === 'win' ? 'lose' : 'win';
         //내 승리여부가 승리일 경우 result는 '패배...', 아닐 경우 '승리!'
     } 
     else { //위 경우가 아니면 props값 그대로 작성
